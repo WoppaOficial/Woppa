8348deb1699906f0e8483ee0c5ebf79104f2bcfb
WoppaOficial <woppaoficial@gmail.com>
2026-07-12 13:28:12 -0500

    Consulta de errores 4

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,3 +4,5 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log("URL de Supabase:", process.env.NEXT_PUBLIC_SUPABASE_URL);
